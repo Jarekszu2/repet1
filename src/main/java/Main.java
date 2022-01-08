92c745a2b554cb72153b30239ece7144f51750af
Jarekszu2 <jarekszu2@onet.pl>
2022-01-08 23:41:06 +0100

    Commit 3.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,8 @@ public class Main {
 
         System.out.println();
         System.out.println("Druga linia.");
+
+        System.out.println();
+        System.out.println("Trzecia linia.");
     }
 }
